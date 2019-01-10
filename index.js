bdc468612655f496a9ec16d7e0a4eb08fe4f6e5c
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-01-10 10:49:58 +0900

    update dependencies and devDependenies
    Highlights:
    
    * update modernizr-stream frmo v1 to v2
    * update ESLint from v3.x to v5.x
    * update @shinnn/eslint-config and follow its rules, for example tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const modernizrStream = require('modernizr-stream');
 const Vinyl = require('vinyl');
 
 module.exports = function vinylModernizr(...args) {
-  const argLen = args.length;
+	const argLen = args.length;
 
-  if (argLen > 1) {
-    const error = new RangeError(`Expected 0 or 1 argument (<Object>), but got ${argLen} arguments.`);
-    error.code = 'ERR_TOO_MANY_ARGS';
+	if (argLen > 1) {
+		const error = new RangeError(`Expected 0 or 1 argument (<Object>), but got ${argLen} arguments.`);
+		error.code = 'ERR_TOO_MANY_ARGS';
 
-    throw error;
-  }
+		throw error;
+	}
 
-  return new Vinyl({path: resolve('modernizr.js'), ...args[0], contents: modernizrStream(...args)});
+	return new Vinyl({path: resolve('modernizr.js'), ...args[0], contents: modernizrStream(...args)});
 };
--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const concatStream = require('concat-stream');
 const {isVinyl} = require('vinyl');
+const {sep} = require('path');
 const test = require('tape');
 const vinylModernizr = require('.');
 
 test('vinylModernizr', t => {
-  t.plan(4);
+	t.plan(4);
 
-  t.ok(isVinyl(vinylModernizr()), 'should create a vinyl instance.');
+	t.ok(isVinyl(vinylModernizr()), 'should create a vinyl instance.');
 
-  const file = vinylModernizr({
-    cwd: 'foo',
-    minify: true
-  });
+	const file = vinylModernizr({
+		cwd: 'foo',
+		minify: true
+	});
 
-  t.equal(file.relative, '../modernizr.js', 'should support Vinyl constructor options.');
+	t.equal(file.relative, `..${sep}modernizr.js`, 'should support Vinyl constructor options.');
 
-  file.contents.pipe(concatStream(data => {
-    t.ok(data.length < 1200, 'should support Modernizr build options.');
-  }));
+	file.contents.on('data', data => {
+		t.ok(data.length < 1200, 'should support Modernizr build options.');
+	});
 
-  t.throws(
-    () => vinylModernizr({}, {}),
-    /^RangeError: Expected 0 or 1 argument \(<Object>\), but got 2 arguments\./u,
-    'should throw an error when it takes too many arguments.'
-  );
+	t.throws(
+		() => vinylModernizr({}, {}),
+		/^RangeError: Expected 0 or 1 argument \(<Object>\), but got 2 arguments\./u,
+		'should throw an error when it takes too many arguments.'
+	);
 });
